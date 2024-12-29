e3be2a0c33984fe2cc8b00600f1476f726a51194
ZaynSaleem <Zainsaleem456@gmail.com>
2024-12-29 22:37:18 +0500

    Heading Uppercase

--- src/components/heading/heading.jsx
+++ src/components/heading/heading.jsx
@@ -1,6 +1,6 @@
 const Heading = ({ title }) => {
   return (
-    <h1 className="text-white font-semibold text-center text-6xl">{title}</h1>
+    <h1 className="text-white font-semibold text-center text-6xl uppercase">{title}</h1>
   );
 };
 
