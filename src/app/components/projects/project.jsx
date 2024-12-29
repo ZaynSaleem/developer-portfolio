98aa9a6da30b5b91c9326ff5eb56cc224b8eb91f
ZaynSaleem <Zainsaleem456@gmail.com>
2024-12-29 22:36:14 +0500

    New projects added, UI fixes, Technologies badge added

--- src/app/components/projects/project.jsx
+++ src/app/components/projects/project.jsx
@@ -1,20 +1,37 @@
 import Heading from "@/components/heading/heading";
 import Image from "next/image";
 import React from "react";
+import { MdArrowForward } from "react-icons/md";
 
 const Projects = () => {
   const projects = [
     {
       title: "OTO Work Website",
       description:
-        "OTO Work is a professional blog website designed to promote its powerful CMS application, the OTO App — an all-in-one solution for managing automated proposals, contracts, and invoices.",
+        "OTO Work is a professional blog website CMS application, the OTO App — an all-in-one solution",
       thumbnailImage: "/assets/projects/otoBlog.jpeg",
+      technologies: ["React Js", "Tailwind CSS", "Strapi"],
     },
     {
       title: "Graphinox Website",
       description:
-        "Graphinox website designed to promote its powerful CMS application, the OTO App — an all-in-one solution for managing automated proposals, contracts, and invoices.",
+        "Graphinox website designed to promote its powerful CMS application, the OTO App — an all-in-one solution",
       thumbnailImage: "/assets/projects/graphinox.jpeg",
+      technologies: ["Next Js", "Tailwind CSS"],
+    },
+    {
+      title: "Budget Collector",
+      description:
+        "Budget Collector website designed to promote its powerful CMS application, the Budget Collector App — an all-in-one solution",
+      thumbnailImage: "/assets/projects/budgetCollector.jpeg",
+      technologies: ["Next Js", "Tailwind CSS", "Rest API", "Mongo DB"],
+    },
+    {
+      title: "AAHMB Real Estate",
+      description:
+        "AAHMB Real Estate website designed to promote its powerful CMS application, the Real Estate App — an all-in-one solution",
+      thumbnailImage: "/assets/projects/aahmbRealEstate.jpeg",
+      technologies: ["Next Js", "Tailwind CSS"],
     },
   ];
 
@@ -22,18 +39,18 @@ const Projects = () => {
     <div className="flex flex-col gap-8 container">
       <Heading title={"PROJECTS"} />
 
-      <div className=" grid grid-cols-2 gap-6">
+      <div className=" grid grid-cols-1 md:grid-cols-2 gap-6">
         {projects && projects?.length
-          ? projects?.map((content, index) => {
+          ? projects?.map((content, outerIndex) => {
               return (
                 <div
-                  key={index}
-                  className="col-span-1 w-full rounded-xl flex flex-col gap-2 overflow-hidden shadow-xl p-4 border border-gray-700"
+                  key={outerIndex}
+                  className="col-span-1 w-full rounded-xl flex flex-col gap-4 overflow-hidden shadow-xl p-4 border bg-[#8e8e8e0f] border-[#e2e8ff1a] box-shadow-header std-backdrop-blur cursor-pointer"
                 >
                   {/* ----------- IMAGE ----------- */}
                   <div className="max-h-56 h-56 bg-slate-400 rounded-xl overflow-hidden relative">
                     <Image
-                      alt={"projectImage-" + index}
+                      alt={"projectImage-" + outerIndex}
                       src={content?.thumbnailImage}
                       layout="fill"
                       priority
@@ -43,14 +60,39 @@ const Projects = () => {
                     />
                   </div>
 
-                  {/* ----------- TITLE ----------- */}
-                  <div className="text-lg font-bold pt-3 capitalize">
-                    {content?.title || "Title"}
-                  </div>
+                  <div className="flex flex-col justify-between gap-2 flex-1">
+                    {/* ----------- TITLE ----------- */}
+                    <div className="text-lg font-bold capitalize">
+                      {content?.title || "Title"}
+                    </div>
 
-                  {/* ----------- DESCRIPTION ----------- */}
-                  <div className="text-gray-300 text-sm">
-                    {content?.description || "Description Here"}
+                    {/* ----------- DESCRIPTION ----------- */}
+                    <div className="text-gray-300 text-sm">
+                      {content?.description || "Description Here"}
+                    </div>
+
+                    {/* ----------- TECHNOLOGIES USED ----------- */}
+                    <div className="flex gap-2 pt-2">
+                      {content &&
+                        content?.technologies?.length &&
+                        content?.technologies?.map((technology, innerIndex) => {
+                          return (
+                            <span
+                              key={innerIndex}
+                              className="text-xs border border-gray-500 text-gray-300 rounded-full py-1 px-3"
+                            >
+                              {technology}
+                            </span>
+                          );
+                        })}
+                    </div>
+
+                    <div className="flex gap-2 items-center pt-3 cursor-pointer group">
+                      <span className="text-sm font-medium">View Project</span>
+                      <span className="rounded-full w-7 h-7 border flex justify-center items-center group-hover:bg-white group-hover:text-black group-hover:-rotate-45 transform transition-all ease-in-out duration-500">
+                        <MdArrowForward size={20} />
+                      </span>
+                    </div>
                   </div>
                 </div>
               );
