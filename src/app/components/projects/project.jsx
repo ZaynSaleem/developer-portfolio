ddf3549613688b64487205d1e5a232bfdf17eda6
ZaynSaleem <Zainsaleem456@gmail.com>
2025-01-16 03:54:45 +0500

    Sub heading key added, pass props to modal, Images key

--- src/app/components/projects/project.jsx
+++ src/app/components/projects/project.jsx
@@ -11,103 +11,123 @@ const Projects = () => {
   const projects = [
     {
       title: "OTO Work Website",
-      description:
+      subheading:
         "OTO Work is a professional blog website CMS application, the OTO App — an all-in-one solution",
+      description:
+        "OTO Work is a professional blog website designed to promote its powerful CMS application, the OTO App — an all-in-one solution for managing automated proposals, contracts, and invoices. The platform showcases key features like easy client management, time tracking, and expense management, offering businesses a seamless way to streamline their workflow. The website was built using React.js for a fast, responsive, and dynamic user experience, while Tailwind CSS ensured a modern, clean, and fully responsive design. I collaborated closely with the client to translate their vision into a stunning design, starting with detailed Figma prototypes and delivering a highly optimized and functional final product. This project not only highlights the OTO App's features but also serves as a hub for valuable content, driving user engagement and interest in the CMS",
       thumbnailImage: "/assets/projects/otoBlog.jpeg",
+      images: [
+        "/assets/projects/otoBlog.jpeg",
+        "/assets/projects/otoWorkFullWebsite.jpeg",
+      ],
       technologies: ["React Js", "Tailwind CSS", "Strapi"],
     },
     {
-      title: "Graphinox Website",
+      title: "Graphinox Agency Website",
+      subheading:
+        "Graphinox is a creative design agency that offers top-notch services such as logo design, animations, and branding solutions.",
       description:
-        "Graphinox website designed to promote its powerful CMS application, the OTO App — an all-in-one solution",
+        "Graphinox is a creative design agency that offers top-notch services such as logo design, animations, and branding solutions. The client’s primary goal was to establish a professional, visually engaging online presence to showcase their services and attract potential clients. project Highlights: - Stunning Visuals - A clean, professional layout with eye-catching animations to reflect the agency’s creative expertise.- Responsive Design - The website is optimized for all devices, ensuring a smooth user experience on mobile, tablet, and desktop. - Service Showcase – Dedicated sections for their core offerings like Logo Design, Animations, UX/UI -Design, and Online Marketing. - Engaging UI/UX – Designed with a focus on user engagement and ease of navigation. - Speed & Performance – Leveraging Next.js for blazing-fast performance and seamless transitions.",
       thumbnailImage: "/assets/projects/graphinox.jpeg",
       technologies: ["Next Js", "Tailwind CSS"],
     },
     {
       title: "Budget Collector",
-      description:
+      subheading:
         "Budget Collector website designed to promote its powerful CMS application, the Budget Collector App — an all-in-one solution",
       thumbnailImage: "/assets/projects/budgetCollector.jpeg",
       technologies: ["Next Js", "Tailwind CSS", "Rest API", "Mongo DB"],
     },
     {
       title: "AAHMB Real Estate",
-      description:
+      subheading:
         "AAHMB Real Estate website designed to promote its powerful CMS application, the Real Estate App — an all-in-one solution",
       thumbnailImage: "/assets/projects/aahmbRealEstate.jpeg",
       technologies: ["Next Js", "Tailwind CSS"],
     },
   ];
 
+  const [selectedProject, setSelectedProject] = useState(null);
+
+  const handleProjectSelection = (project) => {
+    setSelectedProject(project);
+    setIsModalOpen(true);
+  };
+
   return (
     <div className="flex flex-col gap-8 container relative">
       <Heading title={"PROJECTS"} />
 
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
-  {projects && projects?.length
-    ? projects?.map((content, outerIndex) => {
-        return (
-          <div
-            key={outerIndex}
-            className="col-span-1 w-full rounded-xl flex flex-col gap-4 overflow-hidden shadow-lg p-4 border bg-[#8e8e8e0f] border-[#e2e8ff1a] box-shadow-header std-backdrop-blur cursor-pointer group transition-transform transform hover:scale-105"
-          >
-            {/* ----------- IMAGE ----------- */}
-            <div className="h-48 sm:h-56 bg-slate-400 rounded-xl overflow-hidden relative">
-              <Image
-                alt={"projectImage-" + outerIndex}
-                src={content?.thumbnailImage}
-                layout="fill"
-                priority
-                objectFit="cover"
-                quality={100}
-                className="opacity-80 transform transition-all ease-in-out duration-700 group-hover:opacity-100 group-hover:scale-110"
-              />
-            </div>
+        {projects && projects?.length
+          ? projects?.map((content, outerIndex) => {
+              return (
+                <div
+                  key={outerIndex}
+                  className="col-span-1 w-full rounded-xl flex flex-col gap-4 overflow-hidden shadow-lg p-4 border bg-[#8e8e8e0f] border-[#e2e8ff1a] box-shadow-header std-backdrop-blur cursor-pointer group transition-transform transform hover:scale-105"
+                >
+                  {/* ----------- IMAGE ----------- */}
+                  <div className="h-48 sm:h-56 bg-slate-400 rounded-xl overflow-hidden relative">
+                    <Image
+                      alt={"projectImage-" + outerIndex}
+                      src={content?.thumbnailImage}
+                      layout="fill"
+                      priority
+                      objectFit="cover"
+                      quality={100}
+                      className="opacity-80 transform transition-all ease-in-out duration-700 group-hover:opacity-100 group-hover:scale-110"
+                    />
+                  </div>
 
-            {/* ----------- CONTENT ----------- */}
-            <div className="flex flex-col justify-between gap-2 flex-1">
-              {/* ----------- TITLE ----------- */}
-              <div className="text-lg font-bold capitalize text-gray-100">
-                {content?.title || "Title"}
-              </div>
+                  {/* ----------- CONTENT ----------- */}
+                  <div className="flex flex-col justify-between gap-2 flex-1">
+                    {/* ----------- TITLE ----------- */}
+                    <div className="text-lg font-bold capitalize text-gray-100">
+                      {content?.title || "Title"}
+                    </div>
 
-              {/* ----------- DESCRIPTION ----------- */}
-              <div className="text-gray-300 text-sm">
-                {content?.description || "Description Here"}
-              </div>
+                    {/* ----------- subheading ----------- */}
+                    <div className="text-gray-300 text-sm">
+                      {content?.subheading || "subheading Here"}
+                    </div>
 
-              {/* ----------- TECHNOLOGIES USED ----------- */}
-              {content?.technologies?.length > 0 && (
-                <div className="flex flex-wrap gap-2 pt-2">
-                  {content?.technologies?.map((technology, innerIndex) => (
-                    <span
-                      key={innerIndex}
-                      className="text-xs border border-gray-500 text-gray-300 rounded-full py-1 px-3"
+                    {/* ----------- TECHNOLOGIES USED ----------- */}
+                    {content?.technologies?.length > 0 && (
+                      <div className="flex flex-wrap gap-2 pt-2">
+                        {content?.technologies?.map(
+                          (technology, innerIndex) => (
+                            <span
+                              key={innerIndex}
+                              className="text-xs border border-gray-500 text-gray-300 rounded-full py-1 px-3"
+                            >
+                              {technology}
+                            </span>
+                          )
+                        )}
+                      </div>
+                    )}
+
+                    {/* ----------- VIEW PROJECT BUTTON ----------- */}
+                    <button
+                      className="flex gap-2 items-center pt-3 cursor-pointer group"
+                      onClick={() => handleProjectSelection(content)}
                     >
-                      {technology}
-                    </span>
-                  ))}
+                      <span className="text-sm font-medium text-gray-200">
+                        View Project
+                      </span>
+                      <span className="rounded-full w-7 h-7 border border-gray-500 flex justify-center items-center group-hover:bg-white group-hover:text-black group-hover:-rotate-45 transform transition-all ease-in-out duration-500">
+                        <MdArrowForward size={20} />
+                      </span>
+                    </button>
+                  </div>
                 </div>
-              )}
-
-              {/* ----------- VIEW PROJECT BUTTON ----------- */}
-              <button
-                className="flex gap-2 items-center pt-3 cursor-pointer group"
-                onClick={() => setIsModalOpen(true)}
-              >
-                <span className="text-sm font-medium text-gray-200">View Project</span>
-                <span className="rounded-full w-7 h-7 border border-gray-500 flex justify-center items-center group-hover:bg-white group-hover:text-black group-hover:-rotate-45 transform transition-all ease-in-out duration-500">
-                  <MdArrowForward size={20} />
-                </span>
-              </button>
-            </div>
-          </div>
-        );
-      })
-    : ""}
-</div>
+              );
+            })
+          : ""}
+      </div>
 
       <ProjectDetailModal
+        selectedProject={selectedProject}
         isModalOpen={isModalOpen}
         setIsModalOpen={(value) => setIsModalOpen(value)}
       />
