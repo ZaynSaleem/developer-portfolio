3e7a02d1887a361e04d960c3d7ec80cf7e58e7ba
ZaynSaleem <Zainsaleem456@gmail.com>
2025-01-19 19:57:14 +0500

    zIndex added on Animated Star Bg

--- src/components/animatedStarsCanva/animatedStarsCanva.jsx
+++ src/components/animatedStarsCanva/animatedStarsCanva.jsx
@@ -32,7 +32,7 @@ const StarBackground = (props) => {
 };
 
 const AnimatedStarsCanva = () => (
-  <div className="w-full h-auto fixed inset-0 ">
+  <div className="w-full h-auto fixed inset-0 -z-10">
     <Canvas camera={{ position: [0, 0, 1] }}>
       <Suspense fallback={null}>
         <StarBackground />
