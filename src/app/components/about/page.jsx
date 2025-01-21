72f96ac1d7693d1376c677d139a9e1a0febdbc31
ZaynSaleem <Zainsaleem456@gmail.com>
2025-01-22 03:17:06 +0500

    About content Updated

--- src/app/components/about/page.jsx
+++ src/app/components/about/page.jsx
@@ -2,21 +2,23 @@ import Heading from "@/components/heading/heading";
 
 const About = () => {
   return (
-    <div className="container flex flex-col gap-8 justify-center items-center text-center" id="about">
+    <div
+      className="container flex flex-col gap-14 justify-center items-center text-center"
+      id="about"
+    >
       <Heading title={"About Me"} />
       <p className="text-center">
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged.
+        I am a dedicated and results-driven Software Engineer with over 3 years
+        of experience in building innovative web and mobile applications.
+        Skilled in technologies like React.js, Next.js, Vue.js, and Nest.js, I
+        specialize in creating scalable, user-friendly solutions. I am
+        passionate about problem-solving, client satisfaction, and continuous
+        learning.
         <br />
-        <br />
-        It was popularised in the 1960s with the release of Letraset sheets
-        containing Lorem Ipsum passages, and more recently with desktop
-        publishing software like Aldus PageMaker including versions of Lorem
-        Ipsum
+        As a team lead, I manage and mentor development teams, ensuring smooth
+        project execution and high-quality results. My goal is to drive
+        technical excellence while delivering impactful software that meets both
+        business and user needs.
       </p>
     </div>
   );
